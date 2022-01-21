3e99846a92be1dedb56f6ebe5481ac7bbe142b97
BuildTools <unconfigured@null.spigotmc.org>
2022-01-21 15:22:48 +0100

    Adjusting TradeCommand

--- src/de/mrg4ming/commands/tabcompleter/TradeTabCompleter.java
+++ src/de/mrg4ming/commands/tabcompleter/TradeTabCompleter.java
@@ -10,7 +10,7 @@ import org.bukkit.entity.Player;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TradeTabeCompleter implements TabCompleter {
+public class TradeTabCompleter implements TabCompleter {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
         if(sender instanceof Player) {
