f00c25b86acd8efe5504cff501dbce1ba0e91c8d
BuildTools <unconfigured@null.spigotmc.org>
2022-02-05 13:39:37 +0100

    Adding Trade permissions

--- src/de/mrg4ming/commands/TradeCommand.java
+++ src/de/mrg4ming/commands/TradeCommand.java
@@ -27,7 +27,7 @@ public class TradeCommand implements CommandExecutor {
             Player p = (Player) sender;
 
             if(args.length < 1) {
-                if(p.isOp()) {
+                if(p.hasPermission("tradee.trade.spawn")) {
                     //Spawn Villager
                     Villager v = (Villager) p.getWorld().spawnEntity(p.getLocation(), EntityType.VILLAGER);
                     v.setRemoveWhenFarAway(false);
@@ -40,55 +40,66 @@ public class TradeCommand implements CommandExecutor {
                     p.sendMessage(Main.PREFiX + "§aTrader spawned.");
                 }
             } else if(args.length >= 3){
-                if(args[0].equalsIgnoreCase("create")) {
-                    String _name = args[1];
-                    String _bankAccountName = args[2];
+                if(p.hasPermission("tradee.trade.user")) {
+                    if(args[0].equalsIgnoreCase("create")) {
+                        String _name = args[1];
+                        String _bankAccountName = args[2];
 
-                    if(!Shop.instance.checkIfTradeExists(_name)) {
-                        if(Bank.instance.getIdByName(_bankAccountName) > 0) {
-                            Trade _trade = new Trade(_name,
-                                    0, new OptionItem("§8Placeholder: Product",
-                                    Material.DIRT),
-                                    Trade.Mode.BUY_AND_SELL, 0,
-                                    Bank.instance.accounts.get(Bank.instance.getIdByName(_bankAccountName))
-                            );
+                        if(!Shop.instance.checkIfTradeExists(_name)) {
+                            if(Bank.instance.getIdByName(_bankAccountName) > 0) {
+                                Trade _trade = new Trade(_name,
+                                        0, new OptionItem("§8Placeholder: Product",
+                                        Material.DIRT),
+                                        Trade.Mode.BUY_AND_SELL, 0,
+                                        Bank.instance.accounts.get(Bank.instance.getIdByName(_bankAccountName))
+                                );
 
-                            Shop.tempTrades.put(p.getUniqueId().toString(), _trade);
+                                Shop.tempTrades.put(p.getUniqueId().toString(), _trade);
 
-                            p.openInventory(_trade.getConfigurator().open());
-                        } else {
-                            p.sendMessage(Main.PREFiX + "§cBank account does not exist!");
-                        }
-                    } else {
-                        p.sendMessage(Main.PREFiX + "§cTrade already exists!");
-                    }
-                } else if(args[0].equalsIgnoreCase("remove")) {
-
-                }
-            }else if(args.length == 2){
-                if(args[0].equalsIgnoreCase("remove")) {
-                    String _name = args[1];
-
-                    if(!Shop.instance.checkIfTradeExists(_name)) {
-                        p.sendMessage(Main.PREFiX + "§cTrade does not exist!");
-                        return false;
-                    }
-                    Trade _trade = Shop.instance.getTrade(_name);
-
-                    while(_trade.storage > 0) {
-                        HashMap<Integer, ItemStack> _droppedItems = p.getInventory().addItem(new ItemStack(_trade.getProduct().getType(), _trade.getProductAmount()));
-
-                        if(_droppedItems.size() > 0) {
-                            for(ItemStack i : _droppedItems.values()) {
-                                p.getWorld().dropItem(p.getLocation(), i);
+                                p.openInventory(_trade.getConfigurator().open());
+                            } else {
+                                p.sendMessage(Main.PREFiX + "§cBank account does not exist!");
                             }
+                        } else {
+                            p.sendMessage(Main.PREFiX + "§cTrade already exists!");
                         }
-                        _trade.storage--;
                     }
+                } else {
+                    p.sendMessage(Main.PREFiX + "§4You don't have the permission to use this command!");
+                }
+            } else if(args.length == 2) {
+                if(p.hasPermission("tradee.trade.user")) {
+                    if(args[0].equalsIgnoreCase("remove")) {
+                        String _name = args[1];
 
-                    Shop.instance.removeTrade(Shop.getKeyByValue(Shop.instance.trades, Shop.instance.getTrade(_name)));
-                } else if(args[0].equalsIgnoreCase("changeowner")) {
+                        if(!Shop.instance.checkIfTradeExists(_name)) {
+                            p.sendMessage(Main.PREFiX + "§cTrade does not exist!");
+                            return false;
+                        }
+                        Trade _trade = Shop.instance.getTrade(_name);
 
+                        if(!_trade.getOwner().getOwners().contains(p.getUniqueId().toString())) {
+                            p.sendMessage(Main.PREFiX + "§cYou don't own this trade!");
+                            return true;
+                        }
+
+                        while(_trade.storage > 0) {
+                            HashMap<Integer, ItemStack> _droppedItems = p.getInventory().addItem(new ItemStack(_trade.getProduct().getType(), _trade.getProductAmount()));
+
+                            if(_droppedItems.size() > 0) {
+                                for(ItemStack i : _droppedItems.values()) {
+                                    p.getWorld().dropItem(p.getLocation(), i);
+                                }
+                            }
+                            _trade.storage--;
+                        }
+
+                        Shop.instance.removeTrade(Shop.getKeyByValue(Shop.instance.trades, Shop.instance.getTrade(_name)));
+                    } else if(args[0].equalsIgnoreCase("changeowner")) {
+
+                    }
+                } else {
+                    p.sendMessage(Main.PREFiX + "§4You don't have the permission to use this command!");
                 }
             }
         }
