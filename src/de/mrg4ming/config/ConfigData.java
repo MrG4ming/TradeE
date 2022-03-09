95af895b9ab16916790d2e021f3b83eab8416075
BuildTools <unconfigured@null.spigotmc.org>
2022-03-09 17:04:04 +0100

    Added Javadoc to: ConfigData.java

--- src/de/mrg4ming/config/ConfigData.java
+++ src/de/mrg4ming/config/ConfigData.java
@@ -1,6 +1,14 @@
 package de.mrg4ming.config;
 
 public interface ConfigData {
+
+    /**
+     * Loads the config of this class into the variables of this class.
+     */
     void loadFromConfig();
+
+    /**
+     * Saves the variables of this class into the config of this class.
+     */
     void saveToConfig();
 }
