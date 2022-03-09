d3b6c79bbdb5ba4ebf9bf3c101f1596a733c710f
BuildTools <unconfigured@null.spigotmc.org>
2022-03-09 17:20:00 +0100

    Added Javadoc to: TradeItem.java

--- src/de/mrg4ming/data/trade/TradeItem.java
+++ src/de/mrg4ming/data/trade/TradeItem.java
@@ -16,20 +16,48 @@ import java.util.Map;
 
 public class TradeItem implements ConfigItem {
 
+    /**
+     * The string/char that is used to split the config values (the enchantments e.g.)
+     */
     private static final String CFG_SPLITTER = "-";
 
+    /**
+     * The {@link org.bukkit.inventory.ItemStack} that contains all formatted information of this TradeItem.
+     * <br><br>Use this variable to add this TradeItem to an {@link org.bukkit.inventory.Inventory}.
+     */
     private ItemStack itemStack;
 
+    /**
+     * The material of this TradeItem.
+     */
     private Material material;
+
+    /**
+     * The amount of this TradeItem. <br>(it is not saved in the {@link #itemStack} variable!)
+     */
     private int amount;
+    /**
+     * The {@link org.bukkit.enchantments.Enchantment}'s of the TradeItem.
+     */
     private Map<Enchantment, Integer> enchantments;
 
+    /**
+     * Creates a default TradeItem
+     * <br>({@link #material} = AIR; {@link #amount} = 0; {@link #enchantments} = new HashMap<>()).
+     * <br><br><font color=red>Use this only if you want to load a TradItem from a config!</font>
+     */
     public TradeItem() {
         this.material = Material.AIR;
         this.amount = 0;
         this.enchantments = new HashMap<>();
     }
 
+    /**
+     * Creates a new TradeItem.
+     * @param material the {@link #material}
+     * @param amount the {@link #amount}
+     * @param enchantments {@link #enchantments}
+     */
     public TradeItem(Material material, int amount, Map<Enchantment, Integer> enchantments) {
         this.setMaterial(material);
         this.setAmount(amount);
@@ -114,6 +142,10 @@ public class TradeItem implements ConfigItem {
         return material;
     }
 
+    /**
+     * Sets the {@link #material} for the TradeItem.
+     * @param material the new {@link org.bukkit.Material} of the TradeItem
+     */
     public void setMaterial(Material material) {
         this.itemStack = new ItemStack(material);
         this.material = material;
@@ -123,6 +155,10 @@ public class TradeItem implements ConfigItem {
         return amount;
     }
 
+    /**
+     * Sets the {@link #amount} for the TradeItem.
+     * @param amount the new amount of the TradeItem
+     */
     public void setAmount(int amount) {
         this.amount = amount;
     }
@@ -131,6 +167,11 @@ public class TradeItem implements ConfigItem {
         return enchantments;
     }
 
+    /**
+     * Sets the {@link #enchantments} for the TradeItem and regenerates the {@link #itemStack} of this TradeItem.
+     * <br>Enchantments that don't fit onto the item will be ignored!
+     * @param enchantments the new {@link org.bukkit.enchantments.Enchantment}'s of the TradeItem
+     */
     public void setEnchantments(Map<Enchantment, Integer> enchantments) {
         this.itemStack = new ItemStack(this.material);
 
@@ -161,7 +202,10 @@ public class TradeItem implements ConfigItem {
         this.enchantments = _enchants;
     }
 
-
+    /**
+     * If the {@link #itemStack} is null it will be generated.
+     * @return the {@link #itemStack} of this TradeItem
+     */
     public ItemStack getItemStack() {
         if(itemStack == null) generateItemStack();
         return itemStack;
@@ -177,6 +221,9 @@ public class TradeItem implements ConfigItem {
         this.itemStack = item;
     }
 
+    /**
+     * Generates the {@link #itemStack} of this TradeItem by simply reapplying the enchantments to this TradeItem.
+     */
     private void generateItemStack() {
         this.setEnchantments(this.enchantments);
     }
