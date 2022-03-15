0c5ce75fcd504d8ad79c11f2ca74cb6d1f16fbe4
BuildTools <unconfigured@null.spigotmc.org>
2022-03-15 14:17:21 +0100

    Added Javadoc to: BankAccount.java

--- src/de/mrg4ming/data/BankAccount.java
+++ src/de/mrg4ming/data/BankAccount.java
@@ -7,17 +7,40 @@ import java.util.List;
 
 public class BankAccount {
 
+    /**
+     * The name of the bank account.
+     */
     public String name;
 
+    /**
+     * The owner of the bank account / the UUID's of the players who own this account.
+     * <br><i>Most of the time the account is owned by just one player!</i>
+     */
     private List<String> owners;
+
+    /**
+     * The amount of money the account owns.
+     */
     private double capital;
 
+    /**
+     * Creates a new {@link de.mrg4ming.data.BankAccount}.
+     * @param _name the {@link #name}
+     * @param _owner the owner uuid
+     */
     public BankAccount(String _name, String _owner) {
         owners = new ArrayList<>();
 
         this.name = _name;
         this.owners.add(_owner);
     }
+
+    /**
+     * Creates a new {@link de.mrg4ming.data.BankAccount}.
+     * @param _name the {@link #name}
+     * @param _owner the owner uuid
+     * @param _capital the {@link #capital}
+     */
     public BankAccount(String _name, String _owner, double _capital) {
         owners = new ArrayList<>();
 
@@ -26,6 +49,11 @@ public class BankAccount {
         this.capital = _capital;
     }
 
+    /**
+     * Creates a new {@link de.mrg4ming.data.BankAccount}.
+     * @param _name the {@link #name}
+     * @param _owner the {@link #owners} uuid's
+     */
     public BankAccount(String _name, List<String> _owner) {
         owners = new ArrayList<>();
 
@@ -34,6 +62,13 @@ public class BankAccount {
             this.owners = _owner;
         }
     }
+
+    /**
+     * Creates a new {@link de.mrg4ming.data.BankAccount}.
+     * @param _name the {@link #name}
+     * @param _owner the {@link #owners} uuid's
+     * @param _capital the {@link #capital}
+     */
     public BankAccount(String _name, List<String> _owner, double _capital) {
         owners = new ArrayList<>();
 
@@ -44,14 +79,28 @@ public class BankAccount {
         this.capital = _capital;
     }
 
-
+    /**
+     * Deposits/Adds money to the account.
+     * @param _amount the amount of money
+     */
     public void deposit(double _amount)  {
         this.capital += _amount;
     }
+
+    /**
+     * Withdraws/Subtracts money from the account.
+     * @param _amount the amount of money
+     */
     public void withdraw(double _amount)  {
         this.capital -= _amount;
     }
 
+    /**
+     * Transfers money from this account to the target {@link de.mrg4ming.data.BankAccount}.
+     * @param _target the target {@link de.mrg4ming.data.BankAccount}
+     * @param _amount the amount to transfer
+     * @return false if the target account does not exist
+     */
     public boolean transfer(BankAccount _target, double _amount) {
         if(Bank.instance.accounts.containsValue(_target)) {
             this.withdraw(_amount);
@@ -64,7 +113,7 @@ public class BankAccount {
     public double getCapital() {
         return this.capital;
     }
-    public List getOwners() {
+    public List<String> getOwners() {
         return owners;
     }
 }
